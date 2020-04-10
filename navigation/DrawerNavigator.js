eb0993076315e1b41ed899a3927543ae45c23848
Abdul Azeem <abdul.tech689@gmail.com>
2020-04-10 16:28:58 +0530

    adding icons in drawer navigator menu

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Image} from 'react-native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import HomeScreen from '../screens/HomeScreen';
 import CategoryScreen from '../screens/CategoryScreen';
@@ -9,8 +10,14 @@ export default class DrawerNavigator extends React.Component {
   render(){
     return(
         <Drawer.Navigator initialRouteName = "Home">
-          <Drawer.Screen name="Home" component={HomeScreen} />
-          <Drawer.Screen name="Categories" component={CategoryScreen} />
+          <Drawer.Screen name="Home" component={HomeScreen} options={{drawerIcon: ()=>
+          <Image source={require('../app/images/home.png')}
+          style={{width: 25, height: 25}}/>
+          }}/>
+          <Drawer.Screen name="Categories" component={CategoryScreen} options={{drawerIcon: ()=>
+          <Image source={require('../app/images/categories.png')}
+          style={{width: 25, height: 25}}/>
+          }}/>
         </Drawer.Navigator>
     )
   }
